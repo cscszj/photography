0bf15390fedcd09d85b4f1f913e01a7f01c72189
zhaojun <zhaojun@xiaomadada.com>
2016-12-09 22:20:47 +0800

    20161209

--- WebContent/static/js/register.js
+++ WebContent/static/js/register.js
@@ -5,6 +5,16 @@ $(function () {
   $('#nationality').change(function () {
     if($('#nationality').val() === '海外请填写') {
       $('#abroad')[0].focus();
+      $('#prov').val('');
+      $('#prov').attr('disabled', 'disabled');
+      $('#city').val('');
+      $('#city').attr('disabled', 'disabled');
+    } else {
+      $('#prov')[0].focus();
+      $('#prov').val('');
+      $('#prov').removeAttr('disabled');
+      $('#city').val('');
+      $('#city').removeAttr('disabled');
     }
   });
 });
